82f6e4100d802cb5e12194983a3d98b0aa53c504
Graham Dianaty <gmdianaty@gmail.com>
2019-11-10 22:20:40 -0500

    parity sync
    Signed-off-by: Graham Dianaty <gmdianaty@gmail.com>

--- src/create_game.bat
+++ src/create_game.bat
@@ -2,5 +2,5 @@ echo CREATING GAME
 set gameNamae=%1
 set solutionName=%2
 
-devtools\bin\kpc.exe /%gameNamae% +game +shaders /mksln %solutionName%.sln
+devtools\bin\kpc.exe /%gameNamae% +game +shaders /mksln %solutionName%.sln /VS2019
 pause
